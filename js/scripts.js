713bf44064b104a4f98df9d9080276521945f7ec
Niles O'Neil <nilesoneil@gmail.com>
2022-08-17 20:32:44 -0700

    updated output of user prompts
    co-authored-by: Jason Markus <jasonlmarkus@gmail.com>
    co-authored-by: April Milan <aprilmilanpdx@gmail.com>
    Co-authored-by: Brandon Spear
    <bowdowntotheepicturkey@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,7 @@ function divide(number1, number2) {
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert("The result of your numbers is " + add(number1, number2) + ". " + "The remainder of your numbers is " + subtract(number1, number2) + ". " + "The multiplication of your numbers is " + multiply(number1, number2) + ". " + "The division of your numbers is " + divide(number1, number2) + ".");
+window.alert("The result of your numbers is " + number1 + " + " + number2 + " = " + add(number1, number2) + ". " + "The remainder of your numbers is " + number1 + " - " + number2 + " = " + subtract(number1, number2) + ". " + "The multiplication of your numbers is " + number1 + " * " + number2 + " = " + multiply(number1, number2) + ". " + "The division of your numbers is " + number1 + " / " + number2 + " = " + divide(number1, number2) + ".");
 
 //window.alert("The remainder of your numbers is " + subtract(number1, number2));
 
